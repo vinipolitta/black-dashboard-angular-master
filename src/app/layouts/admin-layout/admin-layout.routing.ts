802e06c9517fc001646a62a1e8764f94c93b8480
vinipolitta <vinicius.politta1@gmail.com>
2022-05-28 09:01:30 -0300

    init

---
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,26 @@
+import { RestaurantComponent } from "src/app/pages/restaurant/restaurant.component";
+import { HomeComponent } from "./../../pages/home/home.component";
+import { Routes } from "@angular/router";
+
+import { DashboardComponent } from "../../pages/dashboard/dashboard.component";
+import { IconsComponent } from "../../pages/icons/icons.component";
+import { MapComponent } from "../../pages/map/map.component";
+import { NotificationsComponent } from "../../pages/notifications/notifications.component";
+import { UserComponent } from "../../pages/user/user.component";
+import { TablesComponent } from "../../pages/tables/tables.component";
+import { TypographyComponent } from "../../pages/typography/typography.component";
+// import { RtlComponent } from "../../pages/rtl/rtl.component";
+
+export const AdminLayoutRoutes: Routes = [
+  { path: "dashboard", component: DashboardComponent },
+  { path: "icons", component: IconsComponent },
+  { path: "maps", component: MapComponent },
+  { path: "notifications", component: NotificationsComponent },
+  { path: "user", component: UserComponent },
+  { path: "tables", component: TablesComponent },
+  { path: "typography", component: TypographyComponent },
+  { path: "home", component: HomeComponent },
+  { path: "restaurant", component: RestaurantComponent },
+
+  // { path: "rtl", component: RtlComponent }
+];
